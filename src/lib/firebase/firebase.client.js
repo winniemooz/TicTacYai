caa53cadd10de8191858331a1945dc26099fadea
Kultida Jumpa <144102516+winniemooz@users.noreply.github.com>
2024-02-20 09:51:03 +0700

    update-firebase

--- src/lib/firebase/firebase.client.js
+++ src/lib/firebase/firebase.client.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
   projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBICKET,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
   appId: import.meta.env.VITE_APPID
 };
