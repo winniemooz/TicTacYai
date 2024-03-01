79f0d0b4a08e103f0a68fef9917c94a4bde3f2bd
PaptawanPanyapanichkul <paptawan.tcs@gmail.com>
2024-03-02 01:53:32 +0700

    add leaderboard
    not finish

---
+++ src/routes/leaderboard/+page.svelte
@@ -0,0 +1,28 @@
+<div class="body flex min-h-dvh w-full flex-col bg-mongoose-100 font-fredoka">
+	<div class=" flex flex-col items-center justify-start gap-5 sm:gap-0 lg:my-0">
+		<img src="/logo2.png" alt="" class=" my-6 sm:w-[65%] lg:mt-0 lg:w-[50%]" />
+		<p class="pb-5 text-4xl font-semibold text-mongoose-700 sm:text-5xl lg:text-6xl">
+			Leaderboards
+		</p>
+		<table class="w-[80%] table-auto text-sm sm:text-xl lg:text-2xl">
+			<thead class=" bg-mongoose-800 text-mongoose-100">
+				<tr>
+					<th class="p-3 lg:p-5">RANK</th>
+					<th>NAME</th>
+					<th>TOTAL WIN</th>
+				</tr>
+			</thead>
+			<tbody class="bg-mongoose-200 text-mongoose-800">
+				<tr>
+					<th class="p-2 lg:p-3">1</th>
+					<th>Player1</th>
+					<th>20</th>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<button
+		class="absolute bottom-0 left-0 m-6 h-[5vh] w-[30%] rounded-full bg-mongoose-900 text-2xl font-medium text-white sm:h-[8vh] sm:text-3xl lg:w-[10%]"
+		>Back</button
+	>
+</div>
