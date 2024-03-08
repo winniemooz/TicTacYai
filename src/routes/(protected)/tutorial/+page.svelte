95195502f4671b6de0ae90c7189e67a1e1a73a3a
Kultida Jumpa <144102516+winniemooz@users.noreply.github.com>
2024-03-09 02:21:54 +0700

    update - responsive

--- src/routes/(protected)/tutorial/+page.svelte
+++ src/routes/(protected)/tutorial/+page.svelte
@@ -31,31 +31,31 @@
 			</div>
 		</div>
 	</div>
-	<div class="char-skills mx-[10%] my-[4vh] flex flex-col items-center gap-10 lg:flex-row">
+	<div class="char-skills mx-[10%] my-[4vh] flex flex-col items-center lg:flex-row">
 		<div class="flex flex-col items-center lg:w-[60%]">
 			<p class="mb-4 flex text-center text-5xl font-bold tracking-wide text-mongoose-700">
 				CHARACTER
 			</p>
-			<div class="char mx-4 grid h-[45vh] grid-cols-3 gap-8 rounded-xl bg-mongoose-200 px-6">
-				<div class="flex flex-col items-center justify-center text-center">
-					<img src="/piglet.png" alt="" />
-					<p class=" mt-4 font-copyduck text-4xl text-[#FF6A6A]">PIGLET</p>
+			<div class="char mx-4 grid h-[50vh] grid-cols-3 gap-8 rounded-xl bg-mongoose-200 px-6">
+				<div class="flex flex-col mt-10 items-center text-center">
+					<img src="/piglet.png" alt="" class=" w-[80%]" />
+					<p class=" mt-4 font-black text-4xl text-[#FF6A6A]">PIGLET</p>
 					<p class=" text-lg font-medium text-mongoose-800">
 						When the player win in the small grid, get 50% chance of getting random skill if player
 						didn’t received skill yet in game.
 					</p>
 				</div>
-				<div class="flex flex-col items-center justify-center text-center">
-					<img src="/Kitty.png" alt="" />
-					<p class=" mt-4 font-copyduck text-4xl text-[#4B79B0]">KITTY</p>
+				<div class="flex flex-col mt-10 items-center text-center">
+					<img src="/Kitty.png" alt="" class=" w-[80%]"/>
+					<p class=" mt-4 font-black text-4xl text-[#4B79B0]">KITTY</p>
 					<p class=" text-lg font-medium text-mongoose-800">
 						When the opponent wins 2 small grid, the player will receive a random Skill 1 time.(once
 						per game)
 					</p>
 				</div>
-				<div class="flex flex-col items-center justify-center text-center">
-					<img src="/Puppy.png" alt="" />
-					<p class=" mt-4 font-copyduck text-4xl text-[#FA8836]">PUPPY</p>
+				<div class="flex flex-col mt-10 items-center text-center">
+					<img src="/Puppy.png" alt="" class=" w-[80%]"/>
+					<p class=" mt-4 font-black text-4xl text-[#FA8836]">PUPPY</p>
 					<p class=" text-lg font-medium text-mongoose-800">
 						Player have a 25% chance to steal a Skill when their opponent gains a Skill. (once per
 						game)
@@ -67,24 +67,24 @@
 			<p class="w-100 mb-4 flex text-center text-5xl font-bold tracking-wide text-mongoose-700">
 				SPECIAL SKILLS
 			</p>
-			<div class="skill mx-4 grid h-[45vh] grid-cols-3 gap-8 rounded-xl bg-mongoose-200 px-6">
-				<div class="flex flex-col items-center justify-center text-center">
+			<div class="skill mx-4 grid h-[50vh] grid-cols-3 gap-8 rounded-xl bg-mongoose-200 px-6">
+				<div class="flex flex-col mt-16 items-center text-center">
 					<img src="/star.png" alt="" class="w-[60%]" />
-					<p class=" mt-4 font-copyduck text-4xl text-[#FF9900]">STAR</p>
+					<p class=" mt-4 font-black text-4xl text-[#FF9900]">STAR</p>
 					<p class=" text-lg font-medium text-mongoose-800">
 						Players can place there symbol on top of their opponent's symbol.
 					</p>
 				</div>
-				<div class="flex flex-col items-center justify-center text-center">
+				<div class="flex flex-col mt-16 items-center text-center">
 					<img src="/no-stopping.png" alt="" class="w-[55%]" />
-					<p class=" mt-4 font-copyduck text-4xl text-[#CB3D3D]">SKIP</p>
+					<p class=" mt-4 font-black text-4xl text-[#CB3D3D]">SKIP</p>
 					<p class=" text-lg font-medium text-mongoose-800">
 						Your opponent is not allowed to play in next turn.
 					</p>
 				</div>
-				<div class="flex flex-col items-center justify-center text-center">
+				<div class="flex flex-col mt-16 items-center text-center">
 					<img src="/Timer.png" alt="" class="w-[60%]" />
-					<p class=" mt-4 font-copyduck text-4xl text-[#513D6C]">TIMER</p>
+					<p class=" mt-4 font-black text-4xl text-[#513D6C]">TIMER</p>
 					<p class=" text-lg font-medium text-mongoose-800">
 						The next turn your opponent play time reduces to 3 seconds.
 					</p>
